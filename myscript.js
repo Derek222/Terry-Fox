d04bf411e32e814a18be305835dcba1a5fa41843
Derek <dragonwu210@gmail.com>
2015-10-15 18:53:23 -0400

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,6 +1,4 @@
-var userchoice = prompt("how old are you");
-if (userchooce < 16) {
-  alert("you are to old to drive");
-} else {
-  alert("Driving is fun!");
+var Question = prompt("What is your name");
+if (Question != Derek) {
+  
 }
